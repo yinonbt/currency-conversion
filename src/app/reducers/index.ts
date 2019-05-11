ffbb6749dbf441cb13fa628a7d6b86c75d3e4832
yinon <yinon.bentarya@gmail.com>
2019-05-11 23:30:13 +0300

    added selectors

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,5 +1,6 @@
 import * as fromAmount from './amount';
 import * as fromCurrency from './currency';
+import * as fromSelectors from '../selectors/selectors'
 import { Currency } from '../models/currency';
 
 export const reducers = {
@@ -10,4 +11,9 @@ export const reducers = {
 export interface State {
   amount: number;
   currencies: Currency[];
+}
+
+export const selectors = {
+  getAmountState: fromSelectors.getAmountState,
+  getCurrnecyRates: fromSelectors.getCurrnecyRates
 }
---
+++ src/app/selectors/selectors.ts
@@ -0,0 +1,5 @@
+import { State } from '../reducers/index';
+
+export const getAmountState = (state: State) => state.amount
+
+export const getCurrnecyRates = (state: State) => state.currencies
